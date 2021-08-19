66d240a7d29a59d8b01b07f6d1d970f3347bfb83
aurassai <b-raychev@yandex.com>
2021-08-19 18:14:45 +0300

    events paths

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,11 +1,34 @@
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-const EventPage = () => {
+const EventPage = ({ evt }) => {
     return (
         <Layout>
-            <h1>My Event</h1>
+            <h1>{evt.name}</h1>
         </Layout>
     );
 };
 
 export default EventPage;
+
+export const getStaticPaths = async () => {
+    const res = await fetch(`${API_URL}/api/events`);
+    const events = await res.json();
+    const paths = events.map((evt) => ({ params: { slug: evt.slug } }));
+
+    return {
+        paths,
+        fallback: false,
+    };
+};
+
+export const getStaticProps = async ({ params: { slug } }) => {
+    const res = await fetch(`${API_URL}/api/events/${slug}`);
+    const events = await res.json();
+    return {
+        props: {
+            evt: events[0],
+        },
+        revalidate: 1,
+    };
+};
